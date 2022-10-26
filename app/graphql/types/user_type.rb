82fd2e32eb12f8f936d7930c62d1addb5ba67cf9
Rodrigo Marques <rodrigomarques1993@gmail.com>
2022-10-26 13:10:50 +0100

    Add "posts" field to "User"

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -2,6 +2,7 @@ class Types::UserType < Types::BaseObject
 
   description "An User instance."
 
+  # Table fields
   field :first_name, String, null: false
   field :last_name, String, null: false
   field :street, String, null: false
@@ -9,7 +10,15 @@ class Types::UserType < Types::BaseObject
   field :city, String, null: false
   field :postcode, String, null: false
   field :country, String, null: false
-
   field :created_at, GraphQL::Types::ISO8601DateTime, null: false
   field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+
+  # Relational fields
+  field :posts, [Types::PostType], null: true
+
+  # Custom fields
+  field :full_address, String, null: false, description: "The full address of the User"
+  def full_address
+    "#{object.street} #{object.number}, #{object.city}, #{object.postcode}, #{object.country}"
+  end
 end
