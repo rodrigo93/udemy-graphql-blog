c9e0e3bf6e4a9db611de7975a8a8fb18d9e57f4d
Rodrigo Marques <rodrigomarques1993@gmail.com>
2022-10-26 13:11:00 +0100

    Add "comments" field to "Post"

--- app/graphql/types/comment_type.rb
+++ app/graphql/types/comment_type.rb
@@ -1,9 +1,12 @@
 class Types::CommentType < Types::BaseObject
   description "A Comment instance"
 
+  # Table fields
   field :body, String, null: false
   field :post_id, ID, null: false
-
   field :created_at, GraphQL::Types::ISO8601DateTime, null: false
   field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+
+  # Relational fields
+  field :comments, [Types::CommentType], null: true
 end
